6dc87051e313bc8045333587cea4daf190252f4b
metzakaria <metzakaria@gmail.com>
2022-05-19 13:12:06 +0100

    ftp modify

--- routes/api_auth.php
+++ routes/api_auth.php
@@ -16,7 +16,6 @@ use App\Http\Controllers\Api\BillsController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-| not working woll
 */
 
 
